71e4030fd5f5bc4ad59c3821ca57738d3f9031f7
Lanlun's <157339051+alnfataa@users.noreply.github.com>
2026-04-17 16:54:31 +0700

    Pertemuan 2 : Penggunaan Operator

---
+++ Pertemuan 2/latihansegipanjang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float luas, p ,l;
+    cout << "masukan panjang : " ;
+    cin >> p ;
+    cout <<"masukan lebar : " ;
+    cin >> l ;
+luas = p * l ;
+cout << "Luas = " << luas << endl;
+}
---
+++ Pertemuan 2/operator.cpp
@@ -0,0 +1,71 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,b ;
+//     system("CLS");
+    
+//     //assingment operator "=", ngasi nilai ke dalam suatu variable
+//     a = 3;
+//     b = 5;
+//     //aritmatika operator , operator untuk operasi matematika
+// //     int tambah = a + b ;
+// //     int kurang = a - b ;
+// //     int kali = a * b ;
+// //     // float bagi = a / b ;
+// //     float bagi = (float)a / (float)b;
+// //     int modulo = a % b ;
+// //     cout << "hasil penjumlahan : " << tambah  << endl;
+// //     cout << "hasil pengurangan : " <<  kurang << endl;
+// //     cout << "hasil perkalian : " << kali  << endl;
+// //     cout << "hasil pembagian : " << bagi  << endl;
+// //     cout << "hasil penjumlahan : " <<  modulo << endl;
+
+// // relational operator membandingkan dua nilai
+
+// //logical operator membandingkan dua nilai boolean
+// // cout << (true && true) << endl;
+// // cout << ((true && false )) << endl;
+// // cout << ((true || false )) << endl;
+// // cout << ((true || false )) << endl;
+
+// // cout << !true << endl ;
+// // cout << !false << endl ;
+
+// // bitwise operator untuk operasi biner 
+//     // cout << (5 & 7) << endl ;
+//     // cout << (5 / 7) << endl ;
+//     // cout << (5 ^ 7) << endl ;
+//     // cout << (~7) << endl ;
+//     // cout << (48 << 2 ) << endl;
+//     // cout << (48 >> 2 ) << endl;
+
+//     // conditional operator 
+
+//     // int max ;
+//     // max = (a > b) ? a : b ;
+//     // cout << "nilai terbesar : "<< max << endl;
+
+//     //short hand 
+
+//     // a += 7 ;
+//     // cout << a << endl;
+
+//     // incerement dn decrement
+
+//     // pre increment , ++ di dpn
+//     // cout << a << endl;
+//     // cout << ++a <<endl;
+
+//     //post increment
+//     // cout << a++ << endl;
+//     // cout << a << endl;
+
+// // decrement
+// //  pre decrement
+// // cout << b << endl;
+// // cout << --b << endl;
+// //post decrement
+// cout << b-- << endl;
+// cout << b << endl;
+// }
+
